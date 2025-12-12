ff727652873e0c60f068cec7112ad4232fc7c0bf
jufonseca08 <juliafonseca1412@gmail.com>
2025-12-12 10:49:05 -0300

    ok

--- Assets/Códigos/CodigoMoedas.cs
+++ Assets/Códigos/CodigoMoedas.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-public class CodigoMoeda : MonoBehaviour
+public class CoinCounter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger entered by: " + other.gameObject.name);
+        //Debug.Log("Trigger entered by: " + other.gameObject.name);
 
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered the trigger!");
+            //Debug.Log("Player entered the trigger!");
             Destroy(gameObject);
         }
-    }
+    }    
 }
